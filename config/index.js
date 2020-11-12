3540271228bbedc05ad3ab30c8c3e334feea6420
Martín Eberhardt <martineberhardt4@gmail.com>
2020-11-12 15:20:02 -0300

    Initial Commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const dbuser = 'martin';
+const dbpassword = 'Eber2020!';
+
+const MONGODB_URI = `mongodb+srv://martin:${dbpassword}@mernapp.rn4e6.mongodb.net/dbmernapp?retryWrites=true&w=majority`;
+
+module.exports = MONGODB_URI;
